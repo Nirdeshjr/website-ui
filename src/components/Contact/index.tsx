1f52f0eb7a15e1eb05c01394bd3af8b20ad09032
Nirdesh <88871037+Nirdeshjr@users.noreply.github.com>
2024-06-05 13:08:15 +0545

    support

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,65 @@
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
 import NewsLatterBox from "./NewsLatterBox";
 
+
+
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/support/", formData);
+      toast(' Message submitted Successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+    });
+      setFormData({ name: "", email: "", message: "" });
+    } catch (error) {
+      console.error("There was a problem with the submission:", error);
+      toast.error('error!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+    });
+    }
+  };
+
   return (
+    <>
+    <ToastContainer />
     <section id="contact" className="relative overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container relative z-10">
         <div className="-mx-4 flex flex-wrap">
@@ -13,7 +71,7 @@ const Contact = () => {
               <p className="mb-12 text-base font-medium text-body-color">
                 Our support team will get back to you ASAP via email.
               </p>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
@@ -25,7 +83,11 @@ const Contact = () => {
                       </label>
                       <input
                         type="text"
+                        id="name"
+                        name="name"
                         placeholder="Enter your name"
+                        value={formData.name}
+                        onChange={handleChange}
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
                     </div>
@@ -40,7 +102,11 @@ const Contact = () => {
                       </label>
                       <input
                         type="email"
+                        id="email"
+                        name="email"
                         placeholder="Enter your email"
+                        value={formData.email}
+                        onChange={handleChange}
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
                     </div>
@@ -54,15 +120,18 @@ const Contact = () => {
                         Your Message
                       </label>
                       <textarea
+                        id="message"
                         name="message"
                         rows={5}
                         placeholder="Enter your Message"
+                        value={formData.message}
+                        onChange={handleChange}
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       ></textarea>
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                    <button type="submit" className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
                       Submit Message
                     </button>
                   </div>
@@ -76,11 +145,9 @@ const Contact = () => {
         </div>
       </div>
     </section>
+    </>
+    
   );
 };
-  
+
 export default Contact;
-
-
-
-
