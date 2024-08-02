f2ff014dce006cdb208b187f2071e0c25d91e96a
Nirdesh <88871037+Nirdeshjr@users.noreply.github.com>
2024-08-02 16:57:41 +0545

    commit

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,24 +1,168 @@
-import { Brand } from "src/types/brand";
 import Image from "next/image";
 import brandsData from "./brandsData";
 import SectionTitle from "../Common/SectionTitle";
 
 const Brands = () => {
   return (
-    <section>
-      <div className="container mb-24">
+    <section className="relative overflow-hidden">
+      <div className="container mb-24 relative z-10">
         <div className="flex flex-col items-center justify-center">
-        <SectionTitle title="Our Working Patners" paragraph="Trusted by them over a year." center/>
+          <SectionTitle
+            title="Our Working Partners"
+            paragraph="Trusted by them over a year."
+            center
+          />
         </div>
         <div className="-my-12 flex flex-wrap">
           <div className="w-full px-4">
             <div className="flex flex-wrap items-center justify-center rounded-sm bg-gray-light px-8 py-8 dark:bg-gray-dark sm:px-10 md:px-[50px] md:py-[40px] xl:p-[50px] 2xl:px-[70px] 2xl:py-[60px]">
               {brandsData.map((brand) => (
-                <SingleBrand key={brand.id} brand={brand} />
+                <div
+                  key={brand.id}
+                  className="flex w-1/2 items-center justify-center px-3 py-[15px] sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6"
+                >
+                  <a
+                    href={brand.href}
+                    target="_blank"
+                    rel="nofollow noreferrer"
+                    className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
+                  >
+                    <Image
+                      src={brand.image}
+                      alt={brand.name}
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </a>
+                </div>
               ))}
             </div>
           </div>
         </div>
+      </div>
+      <div className="absolute inset-0 z-0">
+        <svg
+          width="450"
+          height="556"
+          viewBox="0 0 450 556"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute -top-20 -right-20 opacity-60"
+        >
+          <circle cx="277" cy="63" r="225" fill="url(#paint0_linear)" />
+          <circle cx="17.9997" cy="182" r="18" fill="url(#paint1_radial)" />
+          <circle cx="76.9997" cy="288" r="34" fill="url(#paint2_radial)" />
+          <circle
+            cx="325.486"
+            cy="302.87"
+            r="180"
+            transform="rotate(-37.6852 325.486 302.87)"
+            fill="url(#paint3_linear)"
+          />
+          <circle
+            opacity="0.8"
+            cx="184.521"
+            cy="315.521"
+            r="132.862"
+            transform="rotate(114.874 184.521 315.521)"
+            stroke="url(#paint4_linear)"
+          />
+          <circle
+            opacity="0.8"
+            cx="356"
+            cy="290"
+            r="179.5"
+            transform="rotate(-30 356 290)"
+            stroke="url(#paint5_linear)"
+          />
+          <circle
+            opacity="0.8"
+            cx="191.659"
+            cy="302.659"
+            r="133.362"
+            transform="rotate(133.319 191.659 302.659)"
+            fill="url(#paint6_linear)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="-54.5003"
+              y1="-178"
+              x2="222"
+              y2="288"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4682B4" />
+              <stop offset="1" stopColor="#4682B4" stopOpacity="0" />
+            </linearGradient>
+            <radialGradient
+              id="paint1_radial"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(17.9997 182) rotate(90) scale(18)"
+            >
+              <stop offset="0.145833" stopColor="#4682B4" stopOpacity="0" />
+              <stop offset="1" stopColor="#4682B4" stopOpacity="0.15" />
+            </radialGradient>
+            <radialGradient
+              id="paint2_radial"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(76.9997 288) rotate(90) scale(34)"
+            >
+              <stop offset="0.145833" stopColor="#4682B4" stopOpacity="0" />
+              <stop offset="1" stopColor="#4682B4" stopOpacity="0.15" />
+            </radialGradient>
+            <linearGradient
+              id="paint3_linear"
+              x1="226.775"
+              y1="-66.1548"
+              x2="292.157"
+              y2="351.421"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4682B4" />
+              <stop offset="1" stopColor="#4682B4" stopOpacity="0" />
+            </linearGradient>
+            <linearGradient
+              id="paint4_linear"
+              x1="184.521"
+              y1="182.159"
+              x2="184.521"
+              y2="448.882"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4682B4" />
+              <stop offset="1" stopColor="white" stopOpacity="0" />
+            </linearGradient>
+            <linearGradient
+              id="paint5_linear"
+              x1="356"
+              y1="110"
+              x2="356"
+              y2="470"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4682B4" />
+              <stop offset="1" stopColor="white" stopOpacity="0" />
+            </linearGradient>
+            <linearGradient
+              id="paint6_linear"
+              x1="118.524"
+              y1="29.2497"
+              x2="166.965"
+              y2="338.63"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4682B4" />
+              <stop offset="1" stopColor="#4682B4" stopOpacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
       </div>
     </section>
   );
@@ -26,19 +170,7 @@ const Brands = () => {
 
 export default Brands;
 
-const SingleBrand = ({ brand }: { brand: Brand }) => {
-  const { href, image, name } = brand;
 
-  return (
-    <div className="flex w-1/2 items-center justify-center px-3 py-[15px] sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
-      <a
-        href={href}
-        target="_blank"
-        rel="nofollow noreferrer"
-        className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
-      >
-        <Image src={image} alt={name} layout="fill" objectFit="contain" />
-      </a>
-    </div>
-  );
-};
+
+
+
--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -5,9 +5,131 @@ const Features = () => {
   return (
     <section id="features" className="py-14 md:py-20 lg:py-28 relative overflow-hidden">
       <div className="absolute inset-0 overflow-hidden z-0">
-        <div className="absolute top-0 left-0 right-0 h-1/2 bg-gradient-to-b from-primary to-transparent opacity-80 z-0 transform -skew-y-6"></div>
+        <div className="absolute bottom-0 right-0 h-full w-full overflow-hidden z-0">
+          {/* SVG Background */}
+          <svg
+            className="absolute right-0 bottom-0 w-[450px] h-[278px] opacity-30"
+            viewBox="0 0 450 278" // Adjusted height to match half of the Brands section
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="277" cy="63" r="225" fill="url(#paint0_linear_25:217)" />
+            <circle cx="17.9997" cy="182" r="18" fill="url(#paint1_radial_25:217)" />
+            <circle cx="76.9997" cy="288" r="34" fill="url(#paint2_radial_25:217)" />
+            <circle
+              cx="325.486"
+              cy="302.87"
+              r="180"
+              transform="rotate(-37.6852 325.486 302.87)"
+              fill="url(#paint3_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="184.521"
+              cy="315.521"
+              r="132.862"
+              transform="rotate(114.874 184.521 315.521)"
+              stroke="url(#paint4_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="356"
+              cy="290"
+              r="179.5"
+              transform="rotate(-30 356 290)"
+              stroke="url(#paint5_linear_25:217)"
+            />
+            <circle
+              opacity="0.8"
+              cx="191.659"
+              cy="302.659"
+              r="133.362"
+              transform="rotate(133.319 191.659 302.659)"
+              fill="url(#paint6_linear_25:217)"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_25:217"
+                x1="-54.5003"
+                y1="-178"
+                x2="222"
+                y2="288"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <radialGradient
+                id="paint1_radial_25:217"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(17.9997 182) rotate(90) scale(18)"
+              >
+                <stop offset="0.145833" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0.1" /> {/* Light blue */}
+              </radialGradient>
+              <radialGradient
+                id="paint2_radial_25:217"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(76.9997 288) rotate(90) scale(34)"
+              >
+                <stop offset="0.145833" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0.1" /> {/* Light blue */}
+              </radialGradient>
+              <linearGradient
+                id="paint3_linear_25:217"
+                x1="226.775"
+                y1="-66.1548"
+                x2="292.157"
+                y2="351.421"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint4_linear_25:217"
+                x1="184.521"
+                y1="182.159"
+                x2="184.521"
+                y2="448.882"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="white" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint5_linear_25:217"
+                x1="356"
+                y1="110"
+                x2="356"
+                y2="470"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="white" stopOpacity="0" />
+              </linearGradient>
+              <linearGradient
+                id="paint6_linear_25:217"
+                x1="118.524"
+                y1="29.2497"
+                x2="166.965"
+                y2="338.63"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#4A6CF7" />
+                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
       </div>
-      <div className="container relative z-0 bg-white dark:bg-gray-900 rounded-lg shadow-xl overflow-hidden">
+      <div className="container relative z-10 bg-white dark:bg-gray-900 rounded-lg shadow-xl overflow-hidden">
         <SectionTitle
           title="Main Features"
           paragraph="Get a robust and user-friendly software that solves your business problems"
@@ -21,13 +143,15 @@ const Features = () => {
             <SingleFeature key={feature.id} feature={feature} />
           ))}
         </div> */}
-          <Cardslide />
+        <Cardslide />
       </div>
     </section>
   );
 };
 
 export default Features;
+
+
 
 
 
